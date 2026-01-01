46a4726d090442a565c66967a0e7b0aff2fcee46
smithwrld <smxthwrld@gmail.com>
2026-01-01 23:54:26 +0530

    done tailwind

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
@@ -41,10 +40,7 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
-      }}
+
       {...props}
     />
   );
